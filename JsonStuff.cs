b2e62951883f625624dcdaa70c0a49521befdf1d
Warg5000 <warg5000@gmail.com>
2023-11-26 17:07:14 +0300

    Update JsonStuff.cs

--- JsonStuff.cs
+++ JsonStuff.cs
@@ -5,23 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace WeatherBot
+    
 {
+    
     public class WeatherForecast
+        
     {
+        
         public double Now { get; set; }
         public string Now_dt { get; set; }
         public class Info
+            
         {
+            
             public string Url { get; set; }
             public double Lat { get; set; }
             public double Lon { get; set; }
+            
         }
+        
         public Info info { get; set; }
+        
         public class Fact
+            
         {
+            
             public double Obs_time { get; set; }
             public double Temp { get; set; }
-          
             public string Condition { get; set; }
             public double Wind_speed { get; set; }
             public string Wind_dir { get; set; }
@@ -32,10 +42,15 @@ namespace WeatherBot
             public bool Polar { get; set; }
             public string Season { get; set; }
             public double Wind_gust { get; set; }
+            
         }
+        
         public Fact fact { get; set; }
+        
         public class Forecast
+            
         {
+            
             public string Date { get; set; }
             public double Date_ts { get; set; }
             public double Week { get; set; }
@@ -44,7 +59,10 @@ namespace WeatherBot
             public double Moon_code { get; set; }
             public string Moon_text { get; set; }
             public List<object> Parts { get; set; }
+            
         }
+        
         public Forecast forecast { get; set; }
+        
     }
 }

94ac5dc665b453e5f8b46d38f67b3e7b6fd96642
warg5000 <warg5000@gmail.com>
2023-11-26 14:40:59 +0300

    Work

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-using Telegram.Bot;
-using Telegram.Bots.Extensions.Polling;
+﻿using Telegram.Bot;
 using Telegram.Bot.Types;
-using Telegram.Bot.Exceptions;
-using Telegram.Bot.Polling;
+using WeatherBot;
+using Newtonsoft.Json;
+string KeyReader(string pathToFile)
+{
+    StreamReader reader = new StreamReader(pathToFile);
+    string token = reader.ReadToEnd();
+    return token;
+}
+string pathToYaKey = "G:\\Local repo\\SensetiveData\\YaAPI.env";
+string YaKey = KeyReader(pathToYaKey);
+HttpClient httpClient = new HttpClient();
+httpClient.DefaultRequestHeaders.Add("X-Yandex-API-Key", YaKey);
+using HttpResponseMessage response = await httpClient.GetAsync("https://api.weather.yandex.ru/v2/informers?lat=59.938676&lon=30.314494");
+string content = await response.Content.ReadAsStringAsync();
+WeatherForecast forecast = JsonConvert.DeserializeObject<WeatherForecast>(content);
+string pathToTGtoken = "G:\\Local repo\\SensetiveData\\Token.env";
+string TGtoken = KeyReader(pathToTGtoken);
+string pathToTGid = "G:\\Local repo\\SensetiveData\\UserID.env";
+string userID = KeyReader(pathToTGid);
+var bot = new TelegramBotClient(TGtoken);
 
-string ReadToEnd("G:\Local repo\SensetiveData\Token.env")
-var bot = new TelegramBotClient("6650477462:AAF9ZXvLjpOqN1Tnve1XkN8a2ECtJ05bPLo");
-Message message = await bot.SendTextMessageAsync(384104267,"Здарова, бандиты");
+string targetTime = "14:35";
+string curTime = DateTime.Now.ToShortTimeString();
+
+while (true)
+{
+    if (targetTime == curTime)
+    { Message message = await bot.SendTextMessageAsync(userID, $"Здарова, бандиты, {forecast.fact.Temp}, погода нахуй");
+        Thread.Sleep(60000);
+    }
+
+}
+
+
+

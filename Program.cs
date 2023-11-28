392d5fa2a386ba3b496d00d068afe55d4bea58cd
warg5000 <warg5000@gmail.com>
2023-11-28 17:24:34 +0300

    Work

--- Program.cs
+++ Program.cs
@@ -2,46 +2,74 @@
 using Telegram.Bot.Types;
 using WeatherBot;
 using Newtonsoft.Json;
+using Telegram.Bot.Exceptions;
 
-string KeyReader(string pathToFile)
+class Program
 {
-    StreamReader reader = new StreamReader(pathToFile);
-    string token = reader.ReadToEnd();
-    return token;
-}
-string pathToYaKey = "C:\\SensetiveData\\YaAPI.env";
-string YaKey = KeyReader(pathToYaKey);
-
-string pathToTGtoken = "C:\\SensetiveData\\Token.env";
-string TGtoken = KeyReader(pathToTGtoken);
-
-string pathToTGid = "C:\\SensetiveData\\UserID.env";
-string TGid = KeyReader(pathToTGid);
-
-string pathToTargetTime = "C:\\SensetiveData\\targetTime.env";
-string targetTime = KeyReader(pathToTargetTime);
-
-string pathToCityCoordinates = "C:\\SensetiveData\\lat-lon.env";
-string cityCoordinates = KeyReader(pathToCityCoordinates);
-
-string pathToURL = "C:\\SensetiveData\\URL.env";
-string URL = KeyReader(pathToURL);
-
-HttpClient httpClient = new();
-httpClient.DefaultRequestHeaders.Add("X-Yandex-API-Key", YaKey);
-
-var bot = new TelegramBotClient(TGtoken);
-
-while (true)
-{
-    string curTime = DateTime.Now.ToShortTimeString(); 
-    if (targetTime == curTime)
+    static string KeyReader(string pathToFile)
     {
-        using HttpResponseMessage response = await httpClient.GetAsync($"{URL}?{cityCoordinates}");
-        string content = await response.Content.ReadAsStringAsync();
-        WeatherForecast forecast = JsonConvert.DeserializeObject<WeatherForecast>(content);
-        Message message = await bot.SendTextMessageAsync(TGid, $"Доброе утро, температура воздуха в СПб сейчас {forecast.fact.Temp}");
-        Thread.Sleep(60000);
+        using (StreamReader reader = new StreamReader(pathToFile))
+        {
+            string token = reader.ReadToEnd();
+            return token;
+        }
+    }
+    public static async Task Main(string[] args)
+    {
+        string pathToYaKey = "C:\\SensetiveData\\YaAPI.env";
+        string YaKey = KeyReader(pathToYaKey);
+
+        string pathToTGtoken = "C:\\SensetiveData\\Token.env";
+        string TGtoken = KeyReader(pathToTGtoken);
+
+        string pathToTGid = "C:\\SensetiveData\\UserID.env";
+        string TGid = KeyReader(pathToTGid);
+
+        string pathToTargetTime = "C:\\SensetiveData\\targetTime.env";
+        string targetTime = KeyReader(pathToTargetTime);
+
+        string pathToCityCoordinates = "C:\\SensetiveData\\lat-lon.env";
+        string cityCoordinates = KeyReader(pathToCityCoordinates);
+
+        string pathToURL = "C:\\SensetiveData\\URL.env";
+        string URL = KeyReader(pathToURL);
+
+        HttpClient httpClient = new();
+        httpClient.DefaultRequestHeaders.Add("X-Yandex-API-Key", YaKey);
+
+        var bot = new TelegramBotClient(TGtoken);
+
+        while (true)
+        {
+            string curTime = DateTime.Now.ToShortTimeString();
+            if (targetTime == curTime)
+            {
+
+                using HttpResponseMessage response = await httpClient.GetAsync($"{URL}?{cityCoordinates}");
+                try
+                {
+                    response.EnsureSuccessStatusCode();
+                    string forecastSource = await response.Content.ReadAsStringAsync();
+                    WeatherForecast forecast = JsonConvert.DeserializeObject<WeatherForecast>(forecastSource);
+                    Message message = await bot.SendTextMessageAsync(TGid, $"Доброе утро, температура воздуха в СПб сейчас {forecast.fact.Temp}");
+                    Thread.Sleep(60000);
+                }
+                catch (HttpRequestException e)
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    {
+                        Console.WriteLine("Неверно указаны координаты");
+                    }
+                    Console.WriteLine(e.Message);
+                    Thread.Sleep(30000);
+                }
+                catch (ApiRequestException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Thread.Sleep(30000);
+                }
+            }
+        }
     }
 }
 

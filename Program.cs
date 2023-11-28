3ab30b9be916fe03b6ef17a801557a03157fc903
warg5000 <warg5000@gmail.com>
2023-11-28 14:17:48 +0300

    Work

--- Program.cs
+++ Program.cs
@@ -9,22 +9,22 @@ string KeyReader(string pathToFile)
     string token = reader.ReadToEnd();
     return token;
 }
-string pathToYaKey = "G:\\Local repo\\SensetiveData\\YaAPI.env";
+string pathToYaKey = "C:\\SensetiveData\\YaAPI.env";
 string YaKey = KeyReader(pathToYaKey);
 
-string pathToTGtoken = "G:\\Local repo\\SensetiveData\\Token.env";
+string pathToTGtoken = "C:\\SensetiveData\\Token.env";
 string TGtoken = KeyReader(pathToTGtoken);
 
-string pathToTGid = "G:\\Local repo\\SensetiveData\\UserID.env";
+string pathToTGid = "C:\\SensetiveData\\UserID.env";
 string TGid = KeyReader(pathToTGid);
 
-string pathToTargetTime = "G:\\Local repo\\SensetiveData\\targetTime.env";
+string pathToTargetTime = "C:\\SensetiveData\\targetTime.env";
 string targetTime = KeyReader(pathToTargetTime);
 
-string pathToCityCoordinates = "G:\\Local repo\\SensetiveData\\lat-lon.env";
+string pathToCityCoordinates = "C:\\SensetiveData\\lat-lon.env";
 string cityCoordinates = KeyReader(pathToCityCoordinates);
 
-string pathToURL = "G:\\Local repo\\SensetiveData\\URL.env";
+string pathToURL = "C:\\SensetiveData\\URL.env";
 string URL = KeyReader(pathToURL);
 
 HttpClient httpClient = new();
@@ -37,10 +37,10 @@ while (true)
     string curTime = DateTime.Now.ToShortTimeString(); 
     if (targetTime == curTime)
     {
-        using HttpResponseMessage response = await httpClient.GetAsync($"{URL}{cityCoordinates}");
+        using HttpResponseMessage response = await httpClient.GetAsync($"{URL}?{cityCoordinates}");
         string content = await response.Content.ReadAsStringAsync();
         WeatherForecast forecast = JsonConvert.DeserializeObject<WeatherForecast>(content);
-        Message message = await bot.SendTextMessageAsync(TGid, $"Доброе утро, температура воздуха в СПб сейчас{forecast.fact.Temp}");
+        Message message = await bot.SendTextMessageAsync(TGid, $"Доброе утро, температура воздуха в СПб сейчас {forecast.fact.Temp}");
         Thread.Sleep(60000);
     }
 }
